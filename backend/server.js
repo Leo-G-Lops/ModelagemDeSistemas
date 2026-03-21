dfdae8e67c66d9d2bd0796c086378e5a51ce7924
Leonardo Giacobo Lopes <giacobolopes3@gmail.com>
2026-03-21 14:43:46 -0300

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -12,3 +12,48 @@ app.get("/produtos", function(req,res){
         res.json(resultado)
     });
 });
+
+app.post("/produto",function(req,res){
+    const nome = req.body.nome;
+    const tipo = req.body.tipo;
+
+    db.query("INSERT INTO produto(nome,tipo) VALUES (?,?", [nome,tipo], function(erro,resultado){
+        res.json(resultado);
+    });
+});
+
+app.put("/produto/:id", function(req,res){
+    const id = req.params.id;
+    const {nome,tipo} = req.body;
+
+    db.query("UPDATE produto SET nome=? WHERE id=?", [nome, tipo,id], function(erro,resultado){
+        res.json(resultado)
+    });
+});
+
+app.delete("/produto/:id", function(req,res){
+    const id = req.params.id;
+    
+    db.query("DELETE FROM produto WHERE id=?", [id], function(erro,resultado){
+        res.json(resultado)
+    });
+});
+
+app.get("/estoque", function(req,res){
+    db.query("SELECT * FROM estoque", function(erro,resultado){
+        res.json(resultado)
+    });
+});
+
+app.post("/estoque",function(req,res){
+    const produto_id = req.body.produto_id;
+    const quantidade = req.body.quantidade;
+
+    db.query("INSERT INTO estoque(produto_id,quantidade) VALUES (?,?", [produto_id,quantidade], function(erro,resultado){
+        res.json(resultado);
+    });
+});
+
+app. listen(3000, function(){
+    console.log("Servidor rodando na porta 3000")
+});
